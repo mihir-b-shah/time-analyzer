8e3d2c5caa5dfd3942fc2ac37a05de4457ec149d
mihir-b-shah <46288414+mihir-b-shah@users.noreply.github.com>
2021-01-02 00:55:58 -0600

    bulk scraper

--- analyze/scrape.py
+++ analyze/scrape.py
@@ -5,13 +5,10 @@ from bs4 import BeautifulSoup
 from utils import *
 import re
 
-def _get_html(url):
+def get_html(url):
     req = requests.get(url)
     return req.text if req.status_code == 200 else None
 
-def _get_text(html):
+def get_text(html):
     soup = BeautifulSoup(html, 'html.parser')
     return re.sub(r'[^\\,\\.A-Za-z ]+', '', re.sub(r'\\s+', ' ', soup.get_text()))
-
-def get_text(url):
-    return compose([_get_text, _get_html], url)
